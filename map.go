70abb984cb9b9fec0d942bcf87faeaaa83471400
leiless <38041294+leiless@users.noreply.github.com>
2021-01-24 23:21:37 +0800

    Stash in progress Map work

--- map.go
+++ map.go
@@ -6,6 +6,7 @@
 package cuckoohash
 
 import (
+	"fmt"
 	"math/bits"
 	"time"
 )
@@ -110,4 +111,109 @@ func newMap(debug, expandable bool, bytesPerKey, keysPerBucket, bucketCount uint
 		hasher2:       hasher2,
 		debug:         debug,
 	}, nil
+}
+
+// Clumsy but cheap assertion, mainly used for debugging
+func (m *Map) assert(cond bool) {
+	if m.debug {
+		if !cond {
+			panic("assertion failure")
+		}
+	}
+}
+
+func (m *Map) assertEQ(lhs, rhs interface{}) {
+	if m.debug {
+		if lhs != rhs {
+			panic(fmt.Sprintf("equality assertion failure: lhs: %v rhs: %v", lhs, rhs))
+		}
+	}
+}
+
+func (m *Map) assertNE(lhs, rhs interface{}) {
+	if m.debug {
+		if lhs == rhs {
+			panic(fmt.Sprintf("inequality assertion failure: val: %v", lhs))
+		}
+	}
+}
+
+type kvFunc = func([]byte, []byte) bool
+
+// For each loop(read-only) on every key-value in the map
+// Return true if function completed on all items
+func (m *Map) forEachKV(f kvFunc) bool {
+	for _, bucket := range m.buckets {
+		for _, kv := range bucket {
+			if kv == nil {
+				continue
+			}
+
+			// if len(kv) == 1, it means value is nil, we don't store nil directly at [1]
+			m.assert(len(kv) == 1 || len(kv) == 2)
+			var k, v []byte
+			k = kv[0]
+			if len(kv) == 2 {
+				v = kv[1]
+			}
+			if !f(k, v) {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+// Return a raw hash value
+// uint32 is sufficient in our use case.
+func (m *Map) hash1Raw(key []byte) uint32 {
+	return uint32(m.hasher1.Hash64WithSeed(key, m.seed1))
+}
+
+// Return a masked(according to the bucket power) hash index
+func (m *Map) hash1(key []byte) uint32 {
+	return m.hash1Raw(key) & ((1 << m.bucketPower) - 1)
+}
+
+func (m *Map) hash2Raw(key []byte, h1 uint32) uint32 {
+	hh := m.hasher2.Hash64WithSeed(key, m.seed2)
+	h := uint32(hh)
+	if h == 0 {
+		hh2 := simpleHash(key)
+		h = uint32(hh2)
+		if h == 0 {
+			for hh != 0 {
+				if h = uint32(hh ^ hh2); h != 0 {
+					break
+				}
+				hh >>= 8
+			}
+		}
+		// Let alone if h still zero, since the possibility is rare
+		// Expansion as last resort can help this situation
+	}
+	return h1 ^ h
+}
+
+// Return an alternative hash index to resolve hashing collision
+//	it possibly equals to h1
+//
+// To increase load factor(best effort) of the backing buckets
+//	we should return h2 such that h2 != h1
+//	so the alternative hash index can be differentiated from each other
+//	i.e. relocate to another bucket when hash collided
+//
+// We use XOR to swap between hash index and its alternative hash index
+// 	instead of get two hashes and compare to squeeze out the alternative hash index
+// Which means we need an inverse function such that:
+//		func(input, h1) = h2
+//		func(input, h2) = h1
+// XOR is a good fit here
+func (m *Map) hash2(key []byte, h1 uint32) uint32 {
+	h2 := m.hash2Raw(key, h1) & ((1 << m.bucketPower) - 1)
+	// h2 equals to h1 meaning intermediate h is zero
+	if h2 == h1 && m.bucketPower != 0 {
+		m.zeroHash2Count++
+	}
+	return h2
 }

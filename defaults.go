6bd53f6c7fc08b15e0e288317688bc9fdd2433c7
leiless <38041294+leiless@users.noreply.github.com>
2021-02-21 09:01:56 +0800

    Move nextPowerOfTwo() from defaults.go to utils.go

--- defaults.go
+++ defaults.go
@@ -15,16 +15,3 @@ func init() {
 		masks[i] = (uint64(1) << i) - 1
 	}
 }
-
-// If n already power of 2, return value will be n itself
-// If n is 0 or 0xffffffff, zero is returned
-func nextPowerOfTwo(n uint32) uint32 {
-	n--
-	n |= n >> 1
-	n |= n >> 2
-	n |= n >> 4
-	n |= n >> 8
-	n |= n >> 16
-	n++
-	return n
-}
--- utils.go
+++ utils.go
@@ -9,6 +9,19 @@ func debug(format string, a ...interface{}) {
 	fmt.Printf("[DBG] "+format+"\n", a...)
 }
 
+// If n already power of 2, return value will be n itself
+// If n is 0 or 0xffffffff, zero is returned
+func nextPowerOfTwo(n uint32) uint32 {
+	n--
+	n |= n >> 1
+	n |= n >> 2
+	n |= n >> 4
+	n |= n >> 8
+	n |= n >> 16
+	n++
+	return n
+}
+
 const (
 	BYTE = 1 << (10 * iota)
 	KILOBYTE

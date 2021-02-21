3a3f50d1a6b3de2070a9157ff71e5acdf2eee307
leiless <38041294+leiless@users.noreply.github.com>
2021-02-21 22:12:05 +0800

    Add more tests for Map

--- map_test.go
+++ map_test.go
@@ -1,22 +1,32 @@
 package cuckoohash
 
 import (
+	"crypto/md5"
+	"crypto/rand"
+	"fmt"
 	"github.com/OneOfOne/xxhash"
 	"github.com/dgryski/go-farm"
 	"github.com/stretchr/testify/assert"
+	"io"
 	"testing"
 )
 
 var (
-	h1 = farm.Hash64WithSeed
-	h2 = xxhash.Checksum64S
+	h1       = farm.Hash64WithSeed
+	h2       = xxhash.Checksum64S
+	dummyVal = []byte{0xa, 0xb, 0xc, 0xd, 0xe, 0xf}
 )
 
-func TestNewMap(t *testing.T) {
+func TestMap1(t *testing.T) {
 	m, err := newMap(true, 1, 1, 1, h1, h2, true)
 	assert.Nil(t, err)
 	assert.True(t, m.IsEmpty())
 	assert.Equal(t, 0.0, m.LoadFactor())
+	assert.Nil(t, m.Get(nil))
+	assert.False(t, m.ContainsKey(nil))
+	assert.False(t, m.ContainsKey(nil))
+	assert.Equal(t, m.Get(nil, dummyVal), dummyVal)
+	assert.Equal(t, m.Get(nil, dummyVal), []byte{0xa, 0xb, 0xc, 0xd, 0xe, 0xf})
 	t.Log(m)
 
 	for i := 0; i < 256; i++ {
@@ -77,7 +87,7 @@ func TestNewMap(t *testing.T) {
 	t.Log(m)
 }
 
-func TestNewMap2(t *testing.T) {
+func TestMap2(t *testing.T) {
 	m, err := newMap(true, 1, 1, 1, h1, h2, true)
 	assert.Nil(t, err)
 
@@ -109,5 +119,48 @@ func TestNewMap2(t *testing.T) {
 		oldVal, err := m.Del(k)
 		assert.ErrorIs(t, err, ErrKeyNotFound)
 		assert.Nil(t, oldVal)
+
+		assert.Nil(t, m.Get(k))
+		assert.False(t, m.ContainsKey(k))
+		assert.False(t, m.ContainsValue(k))
+	}
+}
+
+func genRandomBytes(size int) []byte {
+	b := make([]byte, size)
+	n, err := rand.Read(b)
+	if err != nil {
+		panic(err)
+	} else if n != size {
+		panic(fmt.Errorf("%w: %v vs %v", io.ErrShortWrite, n, size))
+	}
+	return b
+}
+
+func TestMap3(t *testing.T) {
+	m, err := newMap(true, md5.Size, 1, 1, h1, h2, true)
+	assert.Nil(t, err)
+
+	var list [][]byte
+	n := 5000
+	for i := 0; i < n; i++ {
+		list = append(list, genRandomBytes(md5.Size))
+		oldVal, err := m.Put(list[i], list[i], true)
+		assert.Nil(t, err)
+		assert.Nil(t, oldVal)
+
+		assert.True(t, m.ContainsKey(list[i]))
+		assert.True(t, m.ContainsValue(list[i]))
+		assert.Equal(t, m.Get(list[i]), list[i])
+	}
+
+	assert.Equal(t, m.count, uint64(n))
+	t.Log(m)
+
+	for i := 0; i < 10000; i++ {
+		k := genRandomBytes(md5.Size)
+		assert.Nil(t, m.Get(k))
+		assert.False(t, m.ContainsKey(k))
+		assert.False(t, m.ContainsValue(k))
 	}
 }

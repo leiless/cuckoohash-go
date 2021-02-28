40e23d3ed107455b2e8c3144058cb90d69829c1d
leiless <38041294+leiless@users.noreply.github.com>
2021-02-28 18:52:50 +0800

    Rewrite Set by Map

--- set.go
+++ set.go
@@ -1,336 +1,77 @@
 /*
  * Highly optimized Cuckoo hash set implementation
+ * LICENSE: MIT
  */
 
 package cuckoohash
 
 import (
-	"bytes"
 	"fmt"
-	"github.com/OneOfOne/xxhash"
-	gofarm "github.com/dgryski/go-farm"
-	"math/bits"
-	"math/rand"
-	"time"
+	"strings"
 )
 
 type Set struct {
-	debug bool
-
-	arr   [][][]byte
-	count uint64
-
-	bytesPerKey   uint32
-	keysPerBucket uint32
-	buckets       uint32
-	bucketsPow    int
-
-	expandable     bool
-	expansionCount uint8
-	zeroHash2Count uint64
-
-	seed1 uint64
-	seed2 uint64
+	Map
 }
 
-func NewCuckooHashSet(bytesPerKey, keysPerBucket, buckets uint32) *Set {
-	return newCuckooHashSet(false, true, bytesPerKey, keysPerBucket, buckets)
+func newSet(debug bool, bytesPerKey, keysPerBucket, bucketCount uint32, hasher1, hasher2 hash64WithSeedFunc, expandable bool) (*Set, error) {
+	m, err := newMap(debug, bytesPerKey, keysPerBucket, bucketCount, hasher1, hasher2, expandable)
+	if err != nil {
+		return nil, err
+	}
+	return &Set{Map: *m}, nil
 }
 
-func newCuckooHashSet(debug, expandable bool, bytesPerKey, keysPerBucket, buckets uint32) *Set {
-	if bytesPerKey <= 0 {
-		bytesPerKey = DefaultBytesPerKey
+func NewSet(bytesPerKey, keysPerBucket, bucketCount uint32, hasher1, hasher2 hash64WithSeedFunc, expandableOpt ...bool) (*Set, error) {
+	expandable := true
+	if n := len(expandableOpt); n > 1 {
+		panic(fmt.Sprintf("at most one `expandableOpt` argument can be passed, got %v", n))
+	} else if n != 0 {
+		expandable = expandableOpt[0]
 	}
-	if keysPerBucket <= 0 {
-		keysPerBucket = DefaultKeysPerBucket
-	}
-	if buckets <= 0 {
-		buckets = DefaultBuckets
-	}
-	buckets1 := nextPowerOfTwo(buckets)
-	if buckets1 == 0 {
-		panic(fmt.Sprintf("buckets too large: %v", buckets))
-	}
-	arr := make([][][]byte, buckets1)
-	for i := range arr {
-		arr[i] = make([][]byte, keysPerBucket)
-	}
-	// [][][data] will be allocated on demand
-	return &Set{
-		debug:         debug,
-		arr:           arr,
-		bytesPerKey:   bytesPerKey,
-		keysPerBucket: keysPerBucket,
-		buckets:       buckets1,
-		bucketsPow:    bits.TrailingZeros(uint(buckets1)),
-		expandable:    expandable,
-		seed1:         uint64(time.Now().UnixNano()),
-		seed2:         uint64(time.Now().UnixNano()),
-	}
-}
-
-// For each key in the hash set
-func (s *Set) forEachKey(fn func([]byte)) {
-	var arr [][]byte
-	for i := range s.arr {
-		arr = s.arr[i]
-		for j := range arr {
-			if arr[j] != nil {
-				fn(arr[j])
-			}
-		}
-	}
-}
-
-func (s *Set) hash1(key []byte) uint32 {
-	return uint32(gofarm.Hash64WithSeed(key, s.seed1) & masks[s.bucketsPow])
-}
-
-// Return alternate hash to resolve hashing collision, it possibly equals to h1
-// To reduce hashing collision, hash 2 function should satisfy:
-// 		h1(key) ^ h2(key) != h1(key)
-// Which means h2(key) shouldn't return zero value(unless bucket size very small, e.g. 1)
-func (s *Set) hash2(key []byte, h1 uint32) uint32 {
-	hh := xxhash.Checksum64S(key, s.seed2)
-	h := hh & masks[s.bucketsPow]
-	if h == 0 {
-		hh2 := simpleHash(key)
-		h = hh2 & masks[s.bucketsPow]
-		if h == 0 {
-			for hh != 0 {
-				h = (hh ^ hh2) & masks[s.bucketsPow]
-				if h != 0 {
-					break
-				}
-				hh >>= 8
-			}
-			// Let alone if h still zero, since the possibility is rare
-			// In such case, expansion is the last resort can help
-		}
-	}
-	if h == 0 && s.bucketsPow != 0 {
-		s.zeroHash2Count++
-	}
-	return h1 ^ uint32(h)
-}
-
-// If given key not found in the set, the fn will be called with (nil, -1) argument
-// Thus caller must check nullability of the first argument of the fn
-//
-// Used for functions which may rewrite key binding
-func (s *Set) keyIndexByKey(key []byte, fn func([][]byte, int) bool) bool {
-	if uint32(len(key)) != s.bytesPerKey {
-		return fn(nil, -1)
-	}
-
-	h1 := s.hash1(key)
-	arr := s.arr[h1]
-	for i := range arr {
-		if arr[i] != nil && bytes.Equal(arr[i], key) {
-			return fn(arr, i)
-		}
-	}
-
-	h2 := s.hash2(key, h1)
-	arr = s.arr[h2]
-	for i := range arr {
-		if arr[i] != nil && bytes.Equal(arr[i], key) {
-			return fn(arr, i)
-		}
-	}
-
-	return fn(nil, -1)
-}
-
-// If given key not found in the set, the fn will be called with (nil) argument
-// Thus caller must check nullability of the first argument of the fn
-//
-// Used for functions which only read key binding
-func (s *Set) keyByKey(key []byte, fn func([]byte) bool) bool {
-	return s.keyIndexByKey(key, func(arr [][]byte, i int) bool {
-		if arr == nil {
-			return fn(nil)
-		}
-		return fn(arr[i])
-	})
-}
-
-func (s *Set) assertCount() {
-	if !s.debug {
-		return
-	}
-
-	if s.buckets != 1<<s.bucketsPow {
-		panic(fmt.Sprintf("buckets and bucketsPow mismatch: %v vs %v", s.buckets, s.bucketsPow))
-	}
-	if s.count > uint64(s.keysPerBucket*s.buckets) {
-		panic(fmt.Sprintf("count overflowed bucket capacity: %v vs %v * %v", s.count, s.keysPerBucket, s.buckets))
-	}
-
-	var count uint64
-	s.forEachKey(func(key []byte) {
-		count++
-	})
-	if count != s.count {
-		panic(fmt.Sprintf("count mismatch: expected %v, got %v", count, s.count))
-	}
+	return newSet(false, bytesPerKey, keysPerBucket, bucketCount, hasher1, hasher2, expandable)
 }
 
 func (s *Set) Clear() {
-	var arr [][]byte
-	for i := range s.arr {
-		arr = s.arr[i]
-		for j := range arr {
-			if arr[j] != nil {
-				arr[j] = nil
-				s.count--
-			}
-		}
-	}
-
-	if s.count != 0 {
-		panic(fmt.Sprintf("Bad count after Clear(): %v", s.count))
-	}
+	s.Map.Clear()
 }
 
 func (s *Set) Count() uint64 {
-	s.assertCount()
-	return s.count
+	return s.Map.Count()
 }
 
 func (s *Set) IsEmpty() bool {
 	return s.Count() == 0
 }
 
-// Return estimated memory in bytes used by arr
 func (s *Set) MemoryInBytes() uint64 {
-	return uint64(s.buckets*s.keysPerBucket) + uint64(s.bytesPerKey)*s.count
+	return s.Map.MemoryInBytes()
 }
 
 func (s *Set) LoadFactor() float64 {
-	return float64(s.count) / float64(s.buckets*s.keysPerBucket)
+	return s.Map.LoadFactor()
 }
 
 func (s *Set) Contains(key []byte) bool {
-	return s.keyByKey(key, func(key []byte) bool {
-		return key != nil
-	})
+	return s.Map.ContainsKey(key)
 }
 
-func (s *Set) Remove(key []byte) bool {
-	return s.keyIndexByKey(key, func(arr [][]byte, i int) bool {
-		if arr == nil {
-			return false
-		}
-		arr[i] = nil
-		s.count--
-		s.assertCount()
-		return true
-	})
+func (s *Set) Del(key []byte) error {
+	_, err := s.Map.Del(key)
+	return err
 }
 
-func (s *Set) add0(key []byte, h uint32) bool {
-	arr := s.arr[h]
-	for i, k := range arr {
-		if k == nil {
-			arr[i] = key
-			s.count++
-			s.assertCount()
-			return true
-		}
+func (s *Set) Put(key []byte, ifAbsentOpt ...bool) error {
+	var ifAbsent bool
+	if n := len(ifAbsentOpt); n > 1 {
+		panic(fmt.Sprintf("at most one `ifAbsentOpt` argument can be passed, got %v", n))
+	} else if n != 0 {
+		ifAbsent = ifAbsentOpt[0]
 	}
-	// Out of luck, arr is full
-	return false
-}
-
-func (s *Set) add1(key []byte) bool {
-	h1 := s.hash1(key)
-	if s.add0(key, h1) {
-		return true
-	}
-	h2 := s.hash2(key, h1)
-	if s.add0(key, h2) {
-		return true
-	}
-	h := uint32(rand.Intn(2))
-	if h == 0 {
-		h = h1
-	} else {
-		h = h2
-	}
-	return s.rehashOrExpand(key, h)
-}
-
-// Return true if key added to the set and previously not in the set
-// false if it already in the set
-// false if the bucket is full(s.expandable = false)
-// You may call Contains() to distinguish between already exists and bucket full if s.expandable = false
-func (s *Set) Add(key []byte) bool {
-	if uint32(len(key)) != s.bytesPerKey {
-		panic(fmt.Sprintf("Cannot add, expected key size %v, got %v", s.bytesPerKey, len(key)))
-	}
-	if s.Contains(key) {
-		return false
-	}
-	return s.add1(key)
-}
-
-// Linearly kick-out elements from existing buckets
-//	and re-add those kicked out elements again into a alternate open positions
-// If that fails, expand the buckets and re-add all elements in the hash set
-func (s *Set) rehashOrExpand(key []byte, h uint32) bool {
-	arr := s.arr[h]
-	var newKey []byte
-	for i := uint32(0); i < s.keysPerBucket; i++ {
-		newKey = key
-		key = arr[i]
-		arr[i] = newKey
-
-		if s.add0(key, s.hash2(key, h)) {
-			return true
-		}
-	}
-	if !s.expandable {
-		// Restore initial swapped key back, key location will be shifted by 1
-		arr[0] = key
-		return false
-	}
-
-	if s.debug {
-		debug("Bucket is full, try to expand %v", s)
-	}
-
-	// After re-add, s.buckets may not equals to s.buckets << 1, i.e. the new hash set expanded again internally.
-	t := newCuckooHashSet(s.debug, true, s.bytesPerKey, s.keysPerBucket, s.buckets<<1)
-	s.forEachKey(func(key []byte) {
-		if ok := t.Add(key); !ok {
-			panic(fmt.Sprintf("Cannot add existing keys to expanded set"))
-		}
-	})
-	if ok := t.Add(key); !ok {
-		panic(fmt.Sprintf("Cannot add new key to expanded set"))
-	}
-	s.replace(t)
-
-	if s.debug {
-		debug("Set expanded, %v", s)
-	}
-	return true
-}
-
-func (s *Set) replace(t *Set) {
-	s.arr = t.arr
-	s.count = t.count
-	s.buckets = t.buckets
-	s.bucketsPow = t.bucketsPow
-	s.expansionCount += 1 + t.expansionCount
-	s.zeroHash2Count += t.zeroHash2Count
-	s.seed1 = t.seed1
-	s.seed2 = t.seed2
-	s.assertCount()
+	_, err := s.Map.Put(key, nil, ifAbsent)
+	return err
 }
 
 func (s *Set) String() string {
-	return fmt.Sprintf("[%T debug=%v, mem=%v, loadFactor=%.2f, count=%v, bytesPerKey=%v, keysPerBucket=%v, buckets=%v, bucketsPow=%v expandable=%v expansionCount=%v zeroHash2Count=%v]",
-		s, s.debug, formatByteSize(s.MemoryInBytes()), s.LoadFactor(), s.count, s.bytesPerKey, s.keysPerBucket, s.buckets, s.bucketsPow, s.expandable, s.expansionCount, s.zeroHash2Count)
+	return strings.ReplaceAll(s.Map.String(), fmt.Sprintf("%T", Map{}), fmt.Sprintf("%T", Set{}))
 }
--- set_test.go
+++ set_test.go
@@ -1,123 +1,43 @@
 package cuckoohash
 
 import (
-	"crypto/md5"
-	"fmt"
-	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
 	"testing"
-	"time"
 )
 
-func TestBaseline1(t *testing.T) {
-	s := newCuckooHashSet(true, false, 1, 1, 1)
-	if !s.IsEmpty() {
-		t.Fatalf("%v should be empty", s)
-	}
-	fmt.Printf("%v\n", s)
-	b := []byte{0}
-	if ok := s.Add(b); !ok {
-		t.Fatalf("Add() shouldn't fail")
-	}
-	if !s.Contains(b) {
-		t.Fatalf("Why %x not in %v", b, s)
-	}
-	if s.Count() != 1 {
-		t.Fatalf("Bad count %v", s.Count())
-	}
-	if s.LoadFactor() != 1.0 {
-		t.Fatalf("%v should full by now", s)
-	}
-	fmt.Printf("%v\n", s)
+func TestSet1(t *testing.T) {
+	s, err := newSet(true, 1, 1, 1, h1, h2, false)
+	assert.Nil(t, err)
+	assert.True(t, s.IsEmpty())
+	t.Log(s)
 
-	if ok := s.Remove(b); !ok {
-		t.Fatalf("Why %x not in %v previously", b, s)
-	}
-	if !s.IsEmpty() {
-		t.Fatalf("%v should be empty", s)
-	}
+	b1 := []byte{0}
+	err = s.Put(b1, true)
+	assert.Nil(t, err)
+	assert.True(t, s.Contains(b1))
+	assert.Equal(t, s.Count(), uint64(1))
+	assert.Equal(t, s.LoadFactor(), 1.0)
+	t.Log(s)
 
-	if ok := s.Add(b); !ok {
-		t.Fatalf("Add() shouldn't fail")
-	}
-	if s.Count() != 1 {
-		t.Fatalf("Bad count %v", s.Count())
-	}
+	err = s.Del(b1)
+	assert.Nil(t, err)
+	assert.True(t, s.IsEmpty())
+
+	err = s.Put(b1, true)
+	assert.Nil(t, err)
+	assert.Equal(t, s.Count(), uint64(1))
+
 	s.Clear()
-	if !s.IsEmpty() {
-		t.Fatalf("%v should be empty", s)
-	}
+	assert.True(t, s.IsEmpty())
 
-	if ok := s.Add(b); !ok {
-		t.Fatalf("Add() shouldn't fail")
-	}
+	err = s.Put(b1, true)
+	assert.Nil(t, err)
+	err = s.Put(b1, true)
+	assert.Nil(t, err)
+
 	b2 := []byte{1}
-	if ok := s.Add(b2); ok {
-		t.Fatalf("Add() should not success, since expandable is false")
-	}
-	if s.Contains(b2) {
-		t.Fatalf("Why %v contains %x", s, b2)
-	}
-	if s.Count() != 1 {
-		t.Fatalf("Bad count %v", s.Count())
-	}
-}
+	err = s.Put(b2, true)
+	assert.ErrorIs(t, err, ErrBucketIsFull)
 
-func TestPerformance(t *testing.T) {
-	keysPerBucket := uint32(16)
-	buckets := uint32(300_000)
-	s := NewCuckooHashSet(md5.Size, keysPerBucket, 1)
-	n := uint32(float64(keysPerBucket*buckets) * 0.66)
-	arr := make([][]byte, n)
-
-	t1 := time.Now()
-	for i := range arr {
-		u, err := uuid.NewRandom()
-		if err != nil {
-			t.Errorf("uuid.NewRandom() fail: %v", err)
-		}
-		sum := md5.Sum([]byte(u.String()))
-		a := make([]byte, len(sum))
-		for i, b := range sum {
-			a[i] = b
-		}
-		arr[i] = a
-	}
-	fmt.Printf("Data populate time spent: %v\n", time.Since(t1))
-
-	t1 = time.Now()
-	for _, v := range arr {
-		if !s.Add(v) {
-			t.Fatalf("%x seems already in set", v)
-		}
-	}
-	fmt.Printf("Add() without optimal buckets, time spent: %v\n", time.Since(t1))
-	fmt.Printf("%v\n", s)
-
-	s1 := NewCuckooHashSet(md5.Size, keysPerBucket, s.buckets)
-	t1 = time.Now()
-	for _, v := range arr {
-		if !s1.Add(v) {
-			t.Fatalf("%x seems already in set", v)
-		}
-	}
-	fmt.Printf("Add() with optimal buckets, time spent: %v\n", time.Since(t1))
-	fmt.Printf("%v\n", s1)
-
-	t1 = time.Now()
-	for _, v := range arr {
-		s.Contains(v)
-	}
-	fmt.Printf("Contains() time spent: %v\n", time.Since(t1))
-
-	t1 = time.Now()
-	for _, v := range arr {
-		if !s.Remove(v) {
-			t.Fatalf("%x not in set", v)
-		}
-	}
-	fmt.Printf("Remove() time spent: %v\n", time.Since(t1))
-	fmt.Printf("%v\n", s)
-	if !s.IsEmpty() {
-		t.Fatalf("%v should be empty by now", s)
-	}
+	assert.Equal(t, s.Count(), uint64(1))
 }
